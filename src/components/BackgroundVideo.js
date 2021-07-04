005044ba1c53e9ee44f84084466c5d0a56c68393
Kingsley Nwaogu <Kingsley@Kingsleys-MacBook.local>
2021-07-04 16:45:45 -0500

    updated video

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -1,12 +1,11 @@
-import "../App.css";
-import React from "react";
-import video from "../videos/pexels3.mp4";
+import '../App.css';
+import React from 'react';
+import video from '../videos/pexels3.mp4';
 
 const BackgroundVideo = () => {
 	return (
-		<video id="background" autoPlay muted>
-			{/* <source src={video} type="video/mp4" /> */}
-			<source src={video} />
+		<video id="background" autoPlay muted playsInline>
+			<source src={video} type="video/mp4" />
 		</video>
 	);
 };
